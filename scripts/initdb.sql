bc1e87bf0c41c9c128cdea7fb76cdfd40cdae1d1
Hombrenieve <hombrenieve@gmail.com>
2022-08-06 10:41:12 +0200

    New Architecture of DB
    The intention is to be able to reset
    counts automatically when first day of month comes.
    Also it would be great to get monthly stats.

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -1,7 +1,13 @@
-create table counts (
+create table config (
 	username VARCHAR(32) PRIMARY KEY,
 	chatId INT NOT NULL,
-	payLimit DOUBLE DEFAULT 180.00,
-	autoReset BOOLEAN DEFAULT TRUE,
-	paid DOUBLE
+	payLimit DOUBLE DEFAULT 180.00
+);
+
+create table counts (
+	txDate DATE NOT NULL,
+	username VARCHAR(32),
+	quantity DOUBLE,
+	PRIMARY KEY(txDate, username),
+	FOREIGN KEY(username) REFERENCES config(username)
 );
