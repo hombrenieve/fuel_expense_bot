b6909668928ed9292042dfcd057265f5992c457d
hombrenieve <hombrenieve@gmail.com>
2023-11-16 11:31:00 +0100

    WIP: Creating simpler data-store
    Remove any dependency with a database by using
    a simpler data store in memory

---
+++ src/dataStore.js
@@ -0,0 +1,31 @@
+
+class dataStore {
+    constructor(user, id) {
+        this.limit = 210
+        this.user = user
+        this.id = id
+    }
+
+    restart() {
+
+    }
+
+    getLimit() {
+
+    }
+    
+    setLimit(newLimit) {
+
+    }
+
+    getAmount() {
+
+    }
+
+    addAmount(amount) {
+
+    }
+
+}
+
+module.exports.dataStore = dataStore
